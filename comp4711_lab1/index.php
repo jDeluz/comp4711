b7380bf2ae11633a3e30c6c23c1e5f82e5e2bc3b
Jon Deluz <jondeluz@hotmail.com>
2016-01-06 10:53:50 -0800

    PHP Functions - Made a Tic Tac Toe game, using php functions.

--- comp4711_lab1/index.php
+++ comp4711_lab1/index.php
@@ -15,7 +15,7 @@
     echo '<br/>';
 
     //PHP Expressions
-    $hoursworked = $_GET['hours']; //Changed from 10 to $_GET
+    $hoursworked = 10;//$_GET['hours']; //Changed from 10 to $_GET
     $rate = 12;
     $total = $hoursworked * $rate;
     //echo 'You owe me $' . $total;
@@ -27,6 +27,62 @@
         $total = $hoursworked * $rate;
     }
     echo ($total > 0) ? 'You owe me '. $total : "You're welcome";
+
+    //PHP Functions - Tic Tac Toe
+    $position = $_GET['board'];
+    $squares = str_split($position); //array of moves
+
+    function winner($token, $position){
+        $won = false;
+        if(($position[0] == $token) &&
+           ($position[1] == $token) &&
+           ($position[2] == $token)){
+            $won = true;
+        } else if(($position[4] == $token) &&
+            ($position[5] == $token) &&
+            ($position[6] == $token)) {
+            $won = true;
+        } else if(($position[7] == $token) &&
+            ($position[8] == $token) &&
+            ($position[9] == $token)) {
+            $won = true;
+        } else if(($position[0] == $token) &&
+            ($position[4] == $token) &&
+            ($position[7] == $token)) {
+            $won = true;
+        } else if(($position[1] == $token) &&
+            ($position[5] == $token) &&
+            ($position[8] == $token)) {
+            $won = true;
+        } else if(($position[3] == $token) &&
+            ($position[6] == $token) &&
+            ($position[9] == $token)) {
+            $won = true;
+        } else if(($position[0] == $token) &&
+            ($position[5] == $token) &&
+            ($position[9] == $token)) {
+            $won = true;
+        }
+        else if(($position[3] == $token) &&
+            ($position[5] == $token) &&
+            ($position[7] == $token)) {
+            $won = true;
+        }
+    return $won;
+    }
+
+    echo '<br/>';
+    if(!isset($_GET['board'])){
+        echo 'No board found';
+    } else {
+        if (winner('x', $squares)) {
+            echo 'X win.';
+        } else if (winner('o', $squares)) {
+            echo 'O wins';
+        } else
+            echo 'No winner yet';
+    }
+
 ?>
 
 </body>
