beaa27a11195b3702d617505e95f025c2c217127
Jon Deluz <jondeluz@hotmail.com>
2016-01-06 10:29:52 -0800

    PHP Selection - If statements and ternary operator.

--- comp4711_lab1/index.php
+++ comp4711_lab1/index.php
@@ -7,15 +7,26 @@
 <body>
 
 <?php
+    //PHP Operators
     $name = 'Jon';
     $what = 'geek';
     $level = 10;
     echo 'Hi, my name is ' . $name ,', and I am a level ' . $level . ' ' . $what;
     echo '<br/>';
+
+    //PHP Expressions
     $hoursworked = 10;
     $rate = 12;
     $total = $hoursworked * $rate;
-    echo 'You owe me $' . $total;
+    //echo 'You owe me $' . $total;
+
+    //PHP Selection
+    if ($hoursworked > 40){
+        $total = $hoursworked * $rate * 1.5;
+    } else {
+        $total = $hoursworked * $rate;
+    }
+    echo ($total > 0) ? 'You owe me '. $total : "You're welcome";
 ?>
 
 </body>
